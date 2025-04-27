dea849514c7423f97525714c63dcd12b41134a52
dirane <diranecode@gmail.com>
2025-04-27 11:44:25 +0100

    ajout du richcolor sur les toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             </AuthProvider>
           </ReactQueryProvider>
         </ThemeProvider>
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html >
   )
